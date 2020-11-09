1fcf27fa2a1027f2ebc61e9a37ddce7f6f5cbb35
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-09 21:08:51 +0300

    Add manager scene loader

---
+++ Packages/UGF.Module.Scenes/Runtime/Loaders.Manager/ManagerSceneLoader.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UGF.Module.Scenes.Runtime.Loaders.Manager
+{
+    public class ManagerSceneLoader : SceneLoader<ISceneInfo>
+    {
+        public bool UnloadUnusedAfterUnload { get; }
+
+        public ManagerSceneLoader(bool unloadUnusedAfterUnload = true)
+        {
+            UnloadUnusedAfterUnload = unloadUnusedAfterUnload;
+        }
+
+        protected override Scene OnLoad(ISceneProvider provider, string id, ISceneInfo info, SceneLoadParameters parameters)
+        {
+            var options = new LoadSceneParameters(parameters.AddMode, parameters.PhysicsMode);
+
+            Scene scene = SceneManager.LoadScene(info.Address, options);
+
+            return scene;
+        }
+
+        protected override async Task<Scene> OnLoadAsync(ISceneProvider provider, string id, ISceneInfo info, SceneLoadParameters parameters)
+        {
+            var options = new LoadSceneParameters(parameters.AddMode, parameters.PhysicsMode);
+
+            AsyncOperation operation = SceneManager.LoadSceneAsync(info.Address, options);
+            Scene scene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
+
+            while (!operation.isDone)
+            {
+                await Task.Yield();
+            }
+
+            return scene;
+        }
+
+        protected override void OnUnload(ISceneProvider provider, string id, Scene scene, ISceneInfo info, SceneUnloadParameters parameters)
+        {
+            SceneUtility.UnloadScene(scene, parameters.Options);
+
+            if (UnloadUnusedAfterUnload)
+            {
+                Resources.UnloadUnusedAssets();
+            }
+        }
+
+        protected override async Task OnUnloadAsync(ISceneProvider provider, string id, Scene scene, ISceneInfo info, SceneUnloadParameters parameters)
+        {
+            AsyncOperation operation = SceneManager.UnloadSceneAsync(scene, parameters.Options);
+
+            while (!operation.isDone)
+            {
+                await Task.Yield();
+            }
+
+            if (UnloadUnusedAfterUnload)
+            {
+                operation = Resources.UnloadUnusedAssets();
+
+                while (!operation.isDone)
+                {
+                    await Task.Yield();
+                }
+            }
+        }
+    }
+}
---
+++ Packages/UGF.Module.Scenes/Runtime/Loaders.Manager/ManagerSceneLoaderAsset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UGF.Module.Scenes.Runtime.Loaders.Manager
+{
+    [CreateAssetMenu(menuName = "UGF/Scenes/Manager Scene Loader", order = 2000)]
+    public class ManagerSceneLoaderAsset : SceneLoaderAssetBase
+    {
+        [SerializeField] private bool m_unloadUnusedAfterUnload;
+
+        public bool UnloadUnusedAfterUnload { get { return m_unloadUnusedAfterUnload; } set { m_unloadUnusedAfterUnload = value; } }
+
+        protected override ISceneLoader OnBuild()
+        {
+            return new ManagerSceneLoader(m_unloadUnusedAfterUnload);
+        }
+    }
+}
