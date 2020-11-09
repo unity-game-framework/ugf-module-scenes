fd46e724b730eeb9a6f0298506f2cbe96c9a61f0
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-09 21:08:18 +0300

    Change scene info asset

--- Packages/UGF.Module.Scenes/Runtime/SceneInfoAsset.cs
+++ Packages/UGF.Module.Scenes/Runtime/SceneInfoAsset.cs
@@ -5,16 +5,16 @@ using UnityEngine.SceneManagement;
 namespace UGF.Module.Scenes.Runtime
 {
     [CreateAssetMenu(menuName = "UGF/Scenes/Scene Info", order = 2000)]
-    public class SceneInfoAsset : SceneInfoAssetBase
+    public partial class SceneInfoAsset : SceneInfoAssetBase
     {
-        [AssetGuid(typeof(Scene))]
-        [SerializeField] private string m_scene;
         [AssetGuid(typeof(SceneLoaderAssetBase))]
         [SerializeField] private string m_loader;
-        [SerializeField] private string m_path;
+        [AssetGuid(typeof(Scene))]
+        [SerializeField] private string m_scene;
+        [SerializeField, Disable] private string m_path;
 
-        public string Scene { get { return m_scene; } set { m_scene = value; } }
         public string Loader { get { return m_loader; } set { m_loader = value; } }
+        public string Scene { get { return m_scene; } set { m_scene = value; } }
         public string Path { get { return m_path; } set { m_path = value; } }
 
         protected override ISceneInfo OnBuild()
