449a18e3359ddb5de09c483afbdcfba3db884f42
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-11 20:35:15 +0300

    Add ManagerSceneSettingsBuildPreprocess

---
+++ Packages/UGF.Module.Scenes/Editor/Loaders.Manager/ManagerSceneSettingsBuildPreprocess.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace UGF.Module.Scenes.Editor.Loaders.Manager
+{
+    internal class ManagerSceneSettingsBuildPreprocess : IPreprocessBuildWithReport
+    {
+        public int callbackOrder { get; }
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            if (ManagerSceneEditorSettings.ScenesAutoUpdate)
+            {
+                ManagerSceneEditorSettings.UpdateScenesFromBuildSettings();
+            }
+        }
+    }
+}
--- Packages/UGF.Module.Scenes/Runtime/SceneUtility.cs
+++ Packages/UGF.Module.Scenes/Runtime/SceneUtility.cs
@@ -7,18 +7,13 @@ namespace UGF.Module.Scenes.Runtime
     public static class SceneUtility
     {
         private static readonly Func<Scene, UnloadSceneOptions, bool> m_unloadSceneInternal;
-        private static readonly Func<int, string> m_getGuidInternal;
 
         static SceneUtility()
         {
             MethodInfo unloadSceneInternal = typeof(SceneManager).GetMethod("UnloadSceneInternal", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static)
                                              ?? throw new ArgumentException("Method not found by the specified name: 'SceneManager.UnloadSceneInternal'.");
 
-            MethodInfo getGuidInternal = typeof(Scene).GetMethod("GetGUIDInternal", BindingFlags.NonPublic | BindingFlags.Static)
-                                         ?? throw new ArgumentException("Method not found by the specified name: 'Scene.GetGUIDInternal'.");
-
             m_unloadSceneInternal = (Func<Scene, UnloadSceneOptions, bool>)unloadSceneInternal.CreateDelegate(typeof(Func<Scene, UnloadSceneOptions, bool>));
-            m_getGuidInternal = (Func<int, string>)getGuidInternal.CreateDelegate(typeof(Func<int, string>));
         }
 
         public static bool UnloadScene(Scene scene, UnloadSceneOptions unloadOptions)
@@ -26,13 +21,6 @@ namespace UGF.Module.Scenes.Runtime
             if (!scene.IsValid()) throw new ArgumentException("Value should be valid.", nameof(scene));
 
             return m_unloadSceneInternal.Invoke(scene, unloadOptions);
-        }
-
-        public static string GetSceneGuid(Scene scene)
-        {
-            if (!scene.IsValid()) throw new ArgumentException("Value should be valid.", nameof(scene));
-
-            return m_getGuidInternal.Invoke(scene.handle);
         }
     }
 }
