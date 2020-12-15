9c0891a80d27da4e435cfb3782eefc338e4475ad
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-15 22:54:48 +0300

    Change tests

--- Assets/UGF.Module.Scenes.Runtime.Tests/TestSceneApplicationAccessComponent.cs
+++ Assets/UGF.Module.Scenes.Runtime.Tests/TestSceneApplicationAccessComponent.cs
@@ -7,12 +7,17 @@ namespace UGF.Module.Scenes.Runtime.Tests
     public class TestSceneApplicationAccessComponent : MonoBehaviour
     {
         [SerializeField] private ApplicationAccessComponent m_application;
+        [SerializeField] private bool m_checkOnAwake;
 
         public ApplicationAccessComponent Application { get { return m_application; } set { m_application = value; } }
+        public bool CheckOnAwake { get { return m_checkOnAwake; } set { m_checkOnAwake = value; } }
 
         private void Awake()
         {
-            Check();
+            if (m_checkOnAwake)
+            {
+                Check();
+            }
         }
 
         private void Start()
