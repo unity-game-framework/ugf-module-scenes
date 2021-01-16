02441d0222a97ec8523338b1164acb02670203a4
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-16 17:01:38 +0300

    Change SceneModuleDescription

--- Packages/UGF.Module.Scenes/Runtime/SceneModuleDescription.cs
+++ Packages/UGF.Module.Scenes/Runtime/SceneModuleDescription.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UGF.Application.Runtime;
 
 namespace UGF.Module.Scenes.Runtime
 {
-    public class SceneModuleDescription : ApplicationModuleDescription, ISceneModuleDescription
+    public partial class SceneModuleDescription : ApplicationModuleDescription, ISceneModuleDescription
     {
         public Dictionary<string, ISceneLoader> Loaders { get; } = new Dictionary<string, ISceneLoader>();
         public Dictionary<string, ISceneInfo> Scenes { get; } = new Dictionary<string, ISceneInfo>();
@@ -14,7 +13,7 @@ namespace UGF.Module.Scenes.Runtime
         IReadOnlyDictionary<string, ISceneLoader> ISceneModuleDescription.Loaders { get { return Loaders; } }
         IReadOnlyDictionary<string, ISceneInfo> ISceneModuleDescription.Scenes { get { return Scenes; } }
 
-        public SceneModuleDescription(Type registerType) : base(registerType)
+        public SceneModuleDescription()
         {
         }
     }
