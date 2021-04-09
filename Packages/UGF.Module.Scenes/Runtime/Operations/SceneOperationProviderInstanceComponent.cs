2d65c90c26877f621021f079b81d4392f6a7f46b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-04-09 18:01:49 +0300

    Update SceneOperationProviderInstanceComponent.cs

--- Packages/UGF.Module.Scenes/Runtime/Operations/SceneOperationProviderInstanceComponent.cs
+++ Packages/UGF.Module.Scenes/Runtime/Operations/SceneOperationProviderInstanceComponent.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 namespace UGF.Module.Scenes.Runtime.Operations
 {
+    [DefaultExecutionOrder(int.MinValue)]
     [AddComponentMenu("Unity Game Framework/Scenes/Scene Operation Provider Instance", 2000)]
     public class SceneOperationProviderInstanceComponent : ProviderInstanceComponent<IProvider<Scene, AsyncOperation>>
     {
