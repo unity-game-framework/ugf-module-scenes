97c5ada98521ebb473b5530bec71c92cbf6440b7
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-06-11 23:22:18 +0300

    Update SceneModuleExtensions.cs

--- Packages/UGF.Module.Scenes/Runtime/SceneModuleExtensions.cs
+++ Packages/UGF.Module.Scenes/Runtime/SceneModuleExtensions.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
 
 namespace UGF.Module.Scenes.Runtime
 {
     public static class SceneModuleExtensions
     {
+        public static ISceneLoadParameters GetDefaultLoadParametersByScene(this ISceneModule sceneModule, string id)
+        {
+            return TryGetDefaultLoadParametersByScene(sceneModule, id, out ISceneLoadParameters parameters) ? parameters : throw new ArgumentException($"Scene load parameters not found by the specified scene id: '{id}'.");
+        }
+
+        public static bool TryGetDefaultLoadParametersByScene(this ISceneModule sceneModule, string id, out ISceneLoadParameters parameters)
+        {
+            if (TryGetLoaderByScene(sceneModule, id, out ISceneLoader loader))
+            {
+                parameters = loader.DefaultLoadParameters;
+                return true;
+            }
+
+            parameters = null;
+            return false;
+        }
+
+        public static ISceneUnloadParameters GetDefaultUnloadParametersByScene(this ISceneModule sceneModule, string id)
+        {
+            return TryGetDefaultUnloadParametersByScene(sceneModule, id, out ISceneUnloadParameters parameters) ? parameters : throw new ArgumentException($"Scene unload parameters not found by the specified scene id: '{id}'.");
+        }
+
+        public static bool TryGetDefaultUnloadParametersByScene(this ISceneModule sceneModule, string id, out ISceneUnloadParameters parameters)
+        {
+            if (TryGetLoaderByScene(sceneModule, id, out ISceneLoader loader))
+            {
+                parameters = loader.DefaultUnloadParameters;
+                return true;
+            }
+
+            parameters = null;
+            return false;
+        }
+
         public static ISceneLoader GetLoaderByScene(this ISceneModule sceneModule, string id)
         {
             return TryGetLoaderByScene(sceneModule, id, out ISceneLoader loader) ? loader : throw new ArgumentException($"Scene loader not found by the specified scene id: '{id}'.");
@@ -15,6 +51,42 @@ namespace UGF.Module.Scenes.Runtime
 
             loader = default;
             return sceneModule.Scenes.TryGet(id, out ISceneInfo scene) && sceneModule.Loaders.TryGet(scene.LoaderId, out loader);
+        }
+
+        public static Scene Load(this ISceneModule sceneModule, string id)
+        {
+            if (sceneModule == null) throw new ArgumentNullException(nameof(sceneModule));
+
+            ISceneLoadParameters parameters = GetDefaultLoadParametersByScene(sceneModule, id);
+
+            return sceneModule.Load(id, parameters);
+        }
+
+        public static Task<Scene> LoadAsync(this ISceneModule sceneModule, string id)
+        {
+            if (sceneModule == null) throw new ArgumentNullException(nameof(sceneModule));
+
+            ISceneLoadParameters parameters = GetDefaultLoadParametersByScene(sceneModule, id);
+
+            return sceneModule.LoadAsync(id, parameters);
+        }
+
+        public static void Unload(this ISceneModule sceneModule, string id, Scene scene)
+        {
+            if (sceneModule == null) throw new ArgumentNullException(nameof(sceneModule));
+
+            ISceneUnloadParameters parameters = GetDefaultUnloadParametersByScene(sceneModule, id);
+
+            sceneModule.Unload(id, scene, parameters);
+        }
+
+        public static Task UnloadAsync(this ISceneModule sceneModule, string id, Scene scene)
+        {
+            if (sceneModule == null) throw new ArgumentNullException(nameof(sceneModule));
+
+            ISceneUnloadParameters parameters = GetDefaultUnloadParametersByScene(sceneModule, id);
+
+            return sceneModule.UnloadAsync(id, scene, parameters);
         }
     }
 }
