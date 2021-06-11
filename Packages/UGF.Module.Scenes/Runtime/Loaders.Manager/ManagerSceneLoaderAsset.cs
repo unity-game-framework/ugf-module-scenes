b97492cb65f287e82c568cf4f1baeb0841eb21dc
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-06-11 23:30:21 +0300

    Update ManagerSceneLoaderAsset.cs

--- Packages/UGF.Module.Scenes/Runtime/Loaders.Manager/ManagerSceneLoaderAsset.cs
+++ Packages/UGF.Module.Scenes/Runtime/Loaders.Manager/ManagerSceneLoaderAsset.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace UGF.Module.Scenes.Runtime.Loaders.Manager
 {
@@ -6,8 +7,8 @@ namespace UGF.Module.Scenes.Runtime.Loaders.Manager
     public class ManagerSceneLoaderAsset : SceneLoaderAsset
     {
         [SerializeField] private bool m_unloadUnusedAfterUnload;
-        [SerializeField] private SceneLoadParameters m_defaultLoadParameters = SceneLoadParameters.Default;
-        [SerializeField] private SceneUnloadParameters m_defaultUnloadParameters = SceneUnloadParameters.Default;
+        [SerializeField] private SceneLoadParameters m_defaultLoadParameters = new SceneLoadParameters(LoadSceneMode.Single, LocalPhysicsMode.None);
+        [SerializeField] private SceneUnloadParameters m_defaultUnloadParameters = new SceneUnloadParameters(UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
 
         public bool UnloadUnusedAfterUnload { get { return m_unloadUnusedAfterUnload; } set { m_unloadUnusedAfterUnload = value; } }
         public SceneLoadParameters DefaultLoadParameters { get { return m_defaultLoadParameters; } }
