250109ccd3d8915f60731be36bd2975d1e4bd952
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-01-06 15:53:26 +0300

    Update SceneExtensions.cs

--- Packages/UGF.Module.Scenes/Runtime/Operations/SceneExtensions.cs
+++ Packages/UGF.Module.Scenes/Runtime/Operations/SceneExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UGF.RuntimeTools.Runtime.Providers;
+﻿using System.Threading.Tasks;
+using UGF.RuntimeTools.Runtime.Providers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,16 @@ namespace UGF.Module.Scenes.Runtime.Operations
 {
     public static class SceneExtensions
     {
+        public static async Task ActivateAsync(this Scene scene)
+        {
+            scene.Activate();
+
+            while (!scene.isLoaded)
+            {
+                await Task.Yield();
+            }
+        }
+
         public static void Activate(this Scene scene, bool clearOperation = true)
         {
             var provider = ProviderInstance.Get<IProvider<Scene, AsyncOperation>>();
